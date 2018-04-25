fbfe628de5a4f82e5c9c099aaee68c45c9e5d877
Christia T <christiatidwell@bellsouth.net>
2018-04-25 02:07:45 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,4 +2,19 @@
 
 
 def my_collect(collection)
+ i = 0
+  While collection > 0
+  yield
+
+end
+
+
+def my_collect(collection)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
