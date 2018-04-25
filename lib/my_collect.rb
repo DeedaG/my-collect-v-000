a883281500a5295bdffaa6170ec21513c9d89c83
Christia T <christiatidwell@bellsouth.net>
2018-04-25 01:56:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,8 @@
 
 def my_collect(pets)
   pets = ["dogs", "cats", "birds", "hamsters"]
-  while pets.size < 4 do |item|
-  item.split(" ").upcase
+  while pets.size < 4 do |pet|
+  pet.split(" ").upcase
   end
 end
 end
