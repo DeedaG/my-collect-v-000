3bacdd80a4bbe7dfa8593a51a858381d26c682bb
Christia T <christiatidwell@bellsouth.net>
2018-04-25 02:03:51 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,5 @@
 
 
 
-def my_collect(pets)
-  counter = 1
-  pets = ["dogs", "cats", "birds", "hamsters"]
-  while pets.size > 1 do |pets|
-  pets.split(" ").upcase
-  counter += 1
-  end
-end
-else
-  puts "All of the pets are missing!"
+def my_collect(collection)
 end
